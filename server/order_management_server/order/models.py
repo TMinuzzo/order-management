1572c33bbb3bd41c8d1486a2c202eb46233fcd1e
Minuzzo <thayna.minuzzo1@hp.com>
2021-04-26 20:53:37 -0300

    Create first draft of webpage. Add initial test on server

--- server/order_management_server/order/models.py
+++ server/order_management_server/order/models.py
@@ -9,6 +9,12 @@ class Customer(models.Model):
         return self.name
 
 
+class Profitability:
+    GREAT = "Rentabilidade Ótima"
+    GOOD = "Rentabilidade Boa"
+    BAD = "Rentabilidade Ruim"
+
+
 class Product(models.Model):
     name = CharField(unique=True, max_length=100)
     price = DecimalField(max_digits=20, decimal_places=2)
@@ -16,3 +22,30 @@ class Product(models.Model):
 
     def _str_(self):
         return self.name
+
+
+def price_validator(price):
+    ''' Verifies if the price is bigger than zero, and has 2 decimal places '''
+    print(price)
+    str_price = str(price)
+    print(len(str_price.split('.')[-1]))
+    if price == 0 or len(str_price.split('.')[-1]) > 2:
+        raise ValueError
+
+
+def compute_profitability(product_name, price):
+    profitability = None
+    original_price = Product.objects.find(name=product_name).price
+    delta = price - original_price
+    percent = original_price * 0.1
+
+    if price > original_price:
+        profitability = Profitability.GREAT
+
+    if percent >= delta:
+        profitability = Profitability.GOOD
+
+    if price <= original_price - percent:
+        profitability = Profitability.BAD
+
+    return profitability
--- server/order_management_server/order/tests.py
+++ server/order_management_server/order/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import price_validator
 
-# Create your tests here.
+
+class APICustomerTest(TestCase):
+    pass
+
+
+class ModelCustomerTest(TestCase):
+
+    def test_price_validator(self):
+        price_one = 15.20
+        price_two = 15.233
+        price_three = 0.00
+
+        # correct value
+        price_validator(price_one)
+
+        # incorrect values
+        self.assertRaises(ValueError, price_validator, price_two)
+        self.assertRaises(ValueError, price_validator, price_three)
