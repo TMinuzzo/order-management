ff325e1a898f857b4196c11e299d7ba46d9a9763
Minuzzo <thayna.minuzzo1@hp.com>
2021-05-01 18:25:59 -0300

    Fix settings.py

--- order_management_server/settings.py
+++ order_management_server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-#SECRET_KEY = 'django-insecure-!*9ise!uu-2ju&uo=yz6ie)3r1(#01%auj!@&djh7l$5am8m3i'
+# SECRET_KEY = 'django-insecure-!*9ise!uu-2ju&uo=yz6ie)3r1(#01%auj!@&djh7l$5am8m3i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
