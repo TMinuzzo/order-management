dcbcb3b2dc485abaa6cbc211f08cccb60ae195f2
Minuzzo <thayna.minuzzo1@hp.com>
2021-05-01 17:53:39 -0300

    Change settings file for deploy

--- order_management_server/settings.py
+++ order_management_server/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!*9ise!uu-2ju&uo=yz6ie)3r1(#01%auj!@&djh7l$5am8m3i'
+SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-!*9ise!uu-2ju&uo=yz6ie)3r1(#01%auj!@&djh7l$5am8m3i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -83,6 +84,13 @@ WSGI_APPLICATION = 'order_management_server.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+
+'''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
@@ -92,7 +100,7 @@ DATABASES = {
         'PORT': '5433',
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
