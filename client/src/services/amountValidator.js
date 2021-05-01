cfd80720c7a25bb145679dc1c23e19d1244d659b
Minuzzo <thayna.minuzzo1@hp.com>
2021-05-01 15:55:41 -0300

    Just the server

--- client/src/services/amountValidator.js
+++
@@ -1,9 +0,0 @@
-// Some products can only be sold in predefined multiples and the amount should be bigger than 0
-export default function amountValidator(product, amount) {
-  if (!Number.isInteger(amount)) return false;
-  if (product.multiple == null && amount > 0) return true;
-  if (amount <= 0) return false;
-  else {
-    return amount % product.multiple == 0;
-  }
-}
