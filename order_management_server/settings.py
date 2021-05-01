74528da81b14c22f5b524a79573d882c7fb75f46
Minuzzo <thayna.minuzzo1@hp.com>
2021-05-01 18:15:58 -0300

    Another fix on settings file

--- order_management_server/settings.py
+++ order_management_server/settings.py
@@ -82,13 +82,14 @@ WSGI_APPLICATION = 'order_management_server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
-
+'''
+DATABASES = {'default': dj_database_url.config()}
 '''
 DATABASES = {
     'default': {
